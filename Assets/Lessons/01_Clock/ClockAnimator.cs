eb6342abd5fbcf926b6a1b2c97007a80623804ff
Zach Johnson <1zachjohnson@gmail.com>
2016-11-07 11:50:45 -0800

    adds analog functionality to clock

--- Assets/Lessons/01_Clock/ClockAnimator.cs
+++ Assets/Lessons/01_Clock/ClockAnimator.cs
@@ -9,14 +9,25 @@ public class ClockAnimator : MonoBehaviour {
 	secondsToDegrees = 360f / 60f;
 
 	public Transform hours, minutes, seconds;
+	public bool analog;
 
 	private void Update(){
-		DateTime time = DateTime.Now;
-		hours.localRotation =
+		if (analog) {
+			TimeSpan timespan = DateTime.Now.TimeOfDay;
+			hours.localRotation = Quaternion.Euler (
+				0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
+			minutes.localRotation = Quaternion.Euler (
+				0f, 0f, (float)timespan.TotalMinutes * -minutesToDegrees);
+			seconds.localRotation = Quaternion.Euler (
+				0f, 0f, (float)timespan.TotalSeconds * -secondsToDegrees);
+		} else {
+			DateTime time = DateTime.Now;
+			hours.localRotation =
 			Quaternion.Euler (0f, 0f, time.Hour * -hoursToDegrees);
-		minutes.localRotation =
+			minutes.localRotation =
 			Quaternion.Euler (0f, 0f, time.Minute * -minutesToDegrees);
-		seconds.localRotation =
+			seconds.localRotation =
 			Quaternion.Euler (0f, 0f, time.Second * -secondsToDegrees);
+		}
 	}
 }
